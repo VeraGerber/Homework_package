a6de397a0d460ef622a8977f1557f841e6d04576
VeraGerber <84279244+VeraGerber@users.noreply.github.com>
2024-09-02 12:01:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from .application.db import people
+from .application import salary
+
 import datetime
 
 if __name__ == '__main__':
-  print('Успешно выполнено с помощью palary.py')
+  print('Успешно выполнено с помощью salary.py')
 
 vremya = datetime.datetime.now()
 print(vremya)
+
+print(people)
+print(salary)
